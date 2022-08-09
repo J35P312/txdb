01c3f3931847a9610a1b0c71c0b81475ba72f14f
Jesper Eisfeldt <jesper.eisfeldt@hasta.scilifelab.se>
2022-08-09 15:44:20 +0200

    modified:   README.md 	modified:   txdb.py

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # txdb
 Build frequency database of transcripts from gtf
+
+# Build
+
+	python txdb.py build input.txt > db.gtf
+
+input.txt is a file containing file path to stringtie gtf
+
+# Query
+
+	python txdb.py build in.gtf db.gtf > query.gtf
+
+# Filter
--- txdb.py
+++ txdb.py
@@ -3,15 +3,34 @@ import query
 import filter
 import sys
 
-if sys.argv[1] not in set(["build","query","filter"]):
+if len(sys.argv) < 2:
+	print("python txdb.py build")
+	print("python txdb.py query")
+	print("python txdb.py filter")
 	print("missing argument")
-	print("python main.py build or query")
+	quit()
+
+if sys.argv[1] not in set(["build","query","filter"]):
+	print("python txdb.py build")
+	print("python txdb.py query")
+	print("python txdb.py filter")
+	print("missing argument")
 	quit()
 
 if sys.argv[1] == "build":
+	if len(sys.argv) < 3:
+		print("missing argument")
+		print("python txdb.py build input.txt")
+		print("input.txt is a file containing path to stringtie gtf")
+
 	build.main(sys.argv[2])
 
 elif sys.argv[1] == "query":
+	if len(sys.argv) < 4:
+		print("missing argument")
+		print("python txdb.py in.gtf db.gtf")
+		print("in.gtf is a stringtie gtf, db.gtf is a database produced through txdb.py build")
+
 	query.main(sys.argv[2],sys.argv[3])
 
 elif sys.argv[1] == "filter":
